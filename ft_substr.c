4c8036d922140241f0253458aea3ab1d79878fe7
Juan Bautista Delorme Pinedo <jdelorme@c2r4s6.42madrid.com>
2023-09-28 19:33:07 +0200

    In progress

---
+++ ft_substr.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_substr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jdelorme <jdelorme@student.42madrid>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/28 16:23:16 by jdelorme          #+#    #+#             */
+/*   Updated: 2023/09/28 19:32:29 by jdelorme         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+//declarar una variable para almacenar la string
+//comprabar que no sea null
+//funcion para comprobar que la start sea menor que len
+//si no lo es establecer todos los valores a 0 y
+//sino reservar memoria con malloc + 1 para establecer unicamente un caracter nulo
+//llamar a strlcpy para copiar la cadena
+//
+//si start cumple con la condicion anterior
+//
+//funcion para calcular si len es mayor que la string,
+//si si, restar stringlen a len y almacenar en nueva variable
+//, para reserver unicamente la memoria necesaria
+//si no, 
+
+#include "libft.h"
